84e7b3df82b691427f636b4090bc263a778e7071
Hassaan Mohammed <hassaanmohammed16@gmail.com>
2023-07-19 15:44:35 -0400

    Different implementation for peripheral
    Using clock cycles to measure time

--- SCOMP_Project_Sum23/AudioMonitor.vhd
+++ SCOMP_Project_Sum23/AudioMonitor.vhd
@@ -7,8 +7,8 @@ library IEEE;
 library lpm;
 
 use IEEE.std_logic_1164.all;
-use IEEE.std_logic_unsigned.all;
 use IEEE.numeric_std.all;
+
 use lpm.lpm_components.all;
 
 entity AudioMonitor is
@@ -25,24 +25,20 @@ end AudioMonitor;
 
 architecture a of AudioMonitor is
 
-	 type state_type is ( filling, full );
-	 type sample_type is array (0 to 15) of std_logic_vector(15 downto 0); -- (sample size tbd)
-
-    signal out_en       : std_logic;
-    signal output_data  : std_logic_vector(15 downto 0);
-	 
-	 signal average	   : natural range 0 to integer'high;
-	 signal next_index 	: natural range 0 to 15;	 							  -- (sample size tbd)
-	 signal last_index	: natural range 0 to 15;								  -- (sample size tbd)
-	 signal sample 	   : sample_type;
-	 signal state		   : state_type;
-
+    signal out_en      			: std_logic;
+    signal parsed_data 			: std_logic_vector(15 downto 0);
+    signal output_data 			: std_logic_vector(15 downto 0);
+	 signal sample_count   		: integer range 0 to 30000000 := 0; -- counter for number of samples
+    signal sum_abs_samples 	: integer; 									-- sum of absolute values of samples
+	 signal avg_abs_samples		: integer;									-- average of absolute values of samples
+	 constant divisor				: integer := 30000000;					-- amount of clock cycles equivalent to 2.5s 
+		
 begin
 
     -- Latch data on rising edge of CS to keep it stable during IN
     process (CS) begin
         if rising_edge(CS) then
-            output_data <= std_logic_vector(to_unsigned(average, 16));
+            output_data <=  std_logic_vector(to_unsigned(avg_abs_samples, output_data'length));
         end if;
     end process;
     -- Drive IO_DATA when needed.
@@ -54,35 +50,24 @@ begin
     -- This template device just copies the input data
     -- to IO_DATA by latching the data every time a new
     -- value is ready.
-    process (RESETN, SYS_CLK)
+process (RESETN, SYS_CLK) -- Process is sensitive to the RESETN and SYS_CLK signals
     begin
-        if (RESETN = '0') then
-		  
-				-- initialize index to add data at
-				next_index <= 0;
-				state <= filling;
-				
+        if (RESETN = '0') then 			
+            parsed_data <= (others => '0'); 
+            sum_abs_samples <= 0; 							-- Reset sum_abs_samples to 0
+            sample_count <= 0; 								-- Reset sample_count to 0
+            avg_abs_samples <= 0;							-- Reset avg_abs_samples to 0
         elsif (rising_edge(AUD_NEW)) then
-		  
-				case state is				
-					when filling =>									-- FILLING - average to point of filling
-						if (next_index = 0) then					-- (sample size tbd)
-							state <= full;								-- indicate full if array is maxed
-							last_index <= 14;							-- specify the last index if the next is 0
-						else
-							last_index <= next_index - 1;			-- specify the last index
-						end if;
-						for i in 0 to 15 loop						-- loop over all size (tbd)
-							if i <= last_index then					-- only add if i < size
-								average <= average + to_integer(unsigned(sample(i)));
-							end if;
-						end loop;
-						average <= average / (last_index + 1);	-- average of size to what has been filled
-						
-					when full =>										-- FULL - average entire array
-						average <= average / 15;
-				end case;
+            parsed_data <= AUD_DATA; 						-- Copy the current audio data into parsed_data
+				
+            sum_abs_samples <= sum_abs_samples + to_integer(signed(parsed_data)); -- Add the absolute value of the parsed data to the running sum
+				
+            sample_count <= sample_count + 1; 							-- Increment the sample_count
+            if sample_count = divisor then 								-- If the sample count has reached the divisor (i.e., we have accumulated enough samples)
+                avg_abs_samples <= sum_abs_samples / divisor; 		-- Calculate the average of absolute sample values
+                sample_count <= 0; 											-- Reset the sample count for the next averaging period
+            end if;
         end if;
     end process;
 
-end a;
+end a; -- End of architecture a
